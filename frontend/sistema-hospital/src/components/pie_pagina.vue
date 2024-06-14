a11c97790e50cbb37b2301201fbdce883e2a249c
AlexisGojo <156947433+alexisgmz99@users.noreply.github.com>
2024-06-14 11:09:45 -0600

    Update pie_pagina.vue

--- frontend/sistema-hospital/src/components/pie_pagina.vue
+++ frontend/sistema-hospital/src/components/pie_pagina.vue
@@ -1,26 +1,39 @@
 <template>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    <footer class="bg-black rounded-lg shadow m-4 dark:bg-black-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">ALexis™</a>. Todos los derechos reservados.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">A cerca de</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Politica de privacidad</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licencia</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline">Contactame soy Alexis:D</a>
-        </li>
-    </ul>
+    <div>
+      <!-- El contenido de la página -->
+      <div style="min-height: 100vh;">
+      </div>
+      
+      <!-- Footer fijo -->
+      <footer class="footer bg-black rounded-lg shadow m-4 dark:bg-black-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">ALexis™</a>. Todos los derechos reservados.</span>
+          <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" class="hover:underline me-4 md:me-6">A cerca de</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline me-4 md:me-6">Politica de privacidad</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline me-4 md:me-6">Licencia</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline">Contactame soy Alexis:D</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </div>
-</footer>
   </template>
-
+  
+  <style>
+  .footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+  </style>
 
   
