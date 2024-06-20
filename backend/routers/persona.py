8c95f9d435b8326f7edb2cc2083204a72e860dd3
Alexis <210013@utxicotepec.edu.mx>
2024-06-20 14:35:25 -0600

    culpo a jhaneth

--- backend/routers/persona.py
+++ backend/routers/persona.py
@@ -1,7 +1,33 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime 
+from typing import Optional
 
 persona=APIRouter()
+
+personas=[]
+class listar_personas(BaseModel):
+    id: int
+    nombre:str 
+    primer_apellido: str 
+    segundo_apellido: Optional[str] 
+    edad: int 
+    fecha_nacimiento: datetime
+    curp: str 
+    tipo_sangre:str 
+    created_at: datetime = datetime.now()
+    estatus: bool=False
+    
+
+
+@persona.get("/")
+
+def bienvenida():
+    return "Bienvenido al api del Sistema"
+
+
 @persona.get("/personas")
+
 
 def helloworld():
     return "hello work putos"
