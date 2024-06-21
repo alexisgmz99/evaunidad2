f47909cb3e1cd97bb5150854df260a5681f46b20
AlexisGojo <156947433+alexisgmz99@users.noreply.github.com>
2024-06-21 13:50:07 -0600

    Update app.py
    holi

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from routers.persona import persona
-
-
-
+from routes.persona import persona
+from routes.usuario import usuario
 
 app= FastAPI()
 app.include_router(persona)
-print("Bienvenido a mi aplicacion")
+print("Bienvenido a mi aplicación")
 
 
+app.include_router(usuario)
+print("Bienvenido a mi aplicación")
 
