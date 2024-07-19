0298df6a4c4752b399dc80bf953f47e0c9d03e49
AlexisGojo <156947433+alexisgmz99@users.noreply.github.com>
2024-07-19 13:26:24 -0600

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
-from routes.persona import persona
-from routes.usuario import usuario
+# from routes.persona import persona
+# from backEnd.routes.userrol import usuario
+from routes.user import user
+from routes.persons import person
+from routes.roles import rol
+from routes.userrol import userrol
 
-app= FastAPI()
-app.include_router(persona)
-print("Bienvenido a mi aplicación")
+app = FastAPI()
+# app.include_router(persona)
+# app.include_router(usuario)
+app.include_router(user)
+app.include_router(person)
+app.include_router(rol)
+app.include_router(userrol)
 
-
-app.include_router(usuario)
-print("Bienvenido a mi aplicación")
-
+print ("Bienvenido a mi aplicación")
